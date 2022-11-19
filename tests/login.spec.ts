e0e5bfee31b7891b54dc39d1c0aae57887f29c0c
Bakhtiyar Zubairov <irookanji@gmail.com>
2022-11-19 22:40:10 +0100

    Fix login page test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,5 +6,5 @@ test.beforeEach(async ({ page }) => {
 
 test('Login page opens', async ({ page }) => {
   await page.locator('[data-testid=LoginIcon]').click()
-  await expect(page).toHaveTitle(/sign-in/)
+  await expect(page).toHaveURL(/.*sign-in/)
 })
