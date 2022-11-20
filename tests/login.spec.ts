807e77187b17597c411384c83c8e0117c857aa6d
Bakhtiyar Zubairov <irookanji@gmail.com>
2022-11-20 20:06:11 +0100

    Add two test for Login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,3 +8,18 @@ test('Login page opens', async ({ page }) => {
   await page.locator('[data-testid=LoginIcon]').click()
   await expect(page).toHaveURL(/.*sign-in/)
 })
+
+test('User can not login with invalid username or password', async ({ page }) => {
+  await page.locator('[data-testid=LoginIcon]').click()
+  await page.locator('id=username').fill('Qwerty')
+  await page.locator('id=password').fill('test123')
+  await page.locator('role=button[name="Sign In"]').click()
+  await expect(page.locator('.MuiAlert-message.css-1w0ym84')).toBeVisible()
+})
+
+test('Username is required to be login', async ({ page }) => {
+  await page.locator('[data-testid=LoginIcon]').click()
+  await page.locator('id=password').fill('test123')
+  await page.locator('role=button[name="Sign In"]').click()
+  await expect(page.locator('#username-helper-text')).toBeVisible()
+})
